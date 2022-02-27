edf67be850bbc7ef4758db153e67be7f0f1a947b
Devendra533 <97461548+Devendra533@users.noreply.github.com>
2022-02-27 13:21:09 +0530

    Added Fibonacci Series logic

---
+++ Logical Programs/Logical Programs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class LogicalPrograms
+    {
+        public void FibonacciSeries()
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3;
+            int i;
+            int number;
+
+            Console.Write(" Enter Number of Element: ");
+            number= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1+" "+n2+" ");
+            for (i = 2; i < number; i++) 
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+        }
+       
+    }
+}
---
+++ Logical Programs/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Logical_Programs;
+
+
+Console.WriteLine("*********** WELCOME TO LOGICAL PROGRAMS ***********");
+LogicalPrograms obj = new LogicalPrograms();
+obj.FibonacciSeries();
+
+
+Console.ReadKey();
